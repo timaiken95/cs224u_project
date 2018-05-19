073d97a5b9f9fd0dec73b31beb87676f2405c580
alyvann <avann@stanford.edu>
2018-05-19 01:22:42 -0700

    Preliminary labelled data in data.json

--- process-data.py
+++ process-data.py
@@ -1,3 +1,4 @@
+
 # Language markers 
 # @s:spa = Spanish
 # @s:eng = English
@@ -7,27 +8,34 @@
 # Untagged words are English except where part of an utterance headed [- spa], in which untagged words are Spanish
 
 import os
+import json
+from unidecode import unidecode
 
 folder = 'miami-data'
 files = os.listdir(folder)
+remove = set(['xxx', 'x', '+<', '[/]', '[//]', '[///]', '+...', '+/.', '+//.', '+"/.', '+"', '+,', '+..?', '+".', '+//?', '+/?', '+/?', '++', '+.', '+!?'])
+
+data = []
 
 for file_name in files:
 	if not file_name.endswith('.cha'): continue
 	with open(folder + '/' + file_name) as f:
 
-
-
 		while True:
 			line = f.readline().strip()
 			if not line: break
 
+			## encode with ascii
+			# line = unidecode(line)
+			# line = line.encode('ascii', 'ignore')
+
 			if line.startswith('@'): continue
 			if line.startswith('%eng'): continue
 
-			pos_line = f.readline().strip()
-			if not pos_line.startswith('%aut'): continue ## then line is only a laugh/cough/etc
+			tags_line = f.readline().strip()
+			if not tags_line.startswith('%aut'): continue ## then line is only a laugh/cough/etc
 
-			pos = pos_line.split(':\t')[1]
+			tags = tags_line.split(':\t')[1]
 			comment = line.split(':\t')[1]
 
 			## determine default language
@@ -42,11 +50,47 @@ for file_name in files:
 			else: 
 				default_lang = 'eng'
 
-			words = comment.split()
-			print(words)
+			all_words = comment.split()
+			pos = tags.split()
 
+			words = []
+			for w in all_words:
+				w = w.strip()
+				if w.find('\x15') > -1: continue
+				if w.find('xxx') > -1: continue
+				if w.find('&') > -1: continue
+				if w in remove: continue
+				# if w.find('.') > -1: continue
+				# if w.find('?') > -1: continue
+				# if w.find('!') > -1: continue
+				w = w.replace('(', '')
+				w = w.replace(')', '')
+				w = w.lower()
 
+				lang = default_lang
+				if w.find('@s:') > -1:
+					w_split = w.split('@s:')
+					w = w_split[0]
+					lang = w_split[1]
 
+				words.append([w, 'insert-pos', lang])
+
+			data.append(words)
+			
+			# i = 0
+			# while i < len(pos):
+			# 	pos_split = pos[i].split('.', 1)
+			# 	if len(pos_split) < 2: pos_tag = pos_split[0]
+			# 	else: pos_tag = pos_split[1]
+			# 	i += 1
+
+			# if len(pos) != len(words): 
+			# 	print(words)
+			# 	print(pos)
+			# 	print('!!')
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
 
 
 
